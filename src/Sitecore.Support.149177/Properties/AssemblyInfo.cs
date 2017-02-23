11e6a11c7e0124094948ad043bcdbc9bc3eb5c70
Alexander Lebedenko <ale@sitecore.net>
2017-02-23 16:58:30 +0200

    Field Validation of Versioned fields does not work in Shared Layout mode in EE

---
+++ src/Sitecore.Support.149177/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.149177")]
+[assembly: AssemblyProduct("Sitecore.Support.149177")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.149177/ValidateFields.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.FieldsValidation
+{
+    using Sitecore.Data.Items;
+    using Sitecore.Data.Validators;
+    using Sitecore.Diagnostics;
+    using Sitecore.ExperienceEditor.Speak.Server.Responses;
+    using Sitecore.ExperienceEditor.Switchers;
+    using System.Collections.Generic;
+    using Sitecore.ExperienceEditor.Speak.Ribbon.Requests.FieldsValidation;
+    using Sitecore.Data.Fields;
+    using Sitecore.ExperienceEditor.Utils;
+
+    public class ValidateFields : Sitecore.ExperienceEditor.Speak.Ribbon.Requests.FieldsValidation.ValidateFields
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            Item item = base.RequestContext.Item;
+            Assert.IsNotNull(item, "Item is null");
+            using (new ClientDatabaseSwitcher(item.Database))
+            {
+                ValidatorCollection validators = ValidatorManager.GetFieldsValidators(ValidatorsMode.ValidatorBar, base.RequestContext.GetControlsToValidate().Keys, item.Database);
+                ValidatorManager.Validate(validators, new ValidatorOptions(true));
+                List<FieldValidationError> list = new List<FieldValidationError>();
+                foreach (BaseValidator validator in validators)
+                {
+                    if (!validator.IsValid && (!Sitecore.Data.ID.IsNullOrEmpty(validator.FieldID)))
+                    {
+                        FieldValidationError error = new FieldValidationError
+                        {
+                            Text = validator.Text,
+                            Title = validator.Name,
+                            FieldId = validator.FieldID.ToString(),
+                            DataSourceId = validator.ItemUri.ItemID.ToString(),
+                            Errors = validator.Errors,
+                            Priority = (int)validator.Result
+                        };
+                        list.Add(error);
+                    }
+                }
+                return new PipelineProcessorResponseValue { Value = list };
+            }
+        }
+    }
+}
